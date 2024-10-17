ef1ffe4aa48277cb0fe65ba9d9c411343a839056
sashapoliti <sas.politi@gmail.com>
2024-10-17 17:04:43 +0200

    exercise aura component

--- force-app/main/default/aura/testContactsAura/testContactsAuraController.js
+++ force-app/main/default/aura/testContactsAura/testContactsAuraController.js
@@ -1,5 +1,13 @@
 ({
-	myAction : function(component, event, helper) {
-		
+	fillTable : function(component, event, helper) {
+		helper.fillTableHelper(component, event);
+	},
+
+	createContact : function(component, event, helper) {
+		helper.createContactHelper(component, event);
+	},
+
+	deleteContact : function(component, event, helper) {
+		helper.deleteContactHelper(component, event);
 	}
 })
--- force-app/main/default/aura/testContactsAura/testContactsAuraHelper.js
+++ force-app/main/default/aura/testContactsAura/testContactsAuraHelper.js
@@ -1,5 +1,58 @@
 ({
-	helperMethod : function() {
-		
+	fillTableHelper : function(component, event) {
+		let action = component.get("c.getContacts");
+		action.setCallback(this, function(response) {
+			var state = response.getState();
+			if (state == "SUCCESS") {
+				component.set("v.contactsEl", response.getReturnValue());
+			}
+		});
+		$A.enqueueAction(action);
+	},
+
+	createContactHelper : function(component, event) {
+		console.log('ciao');
+		console.log(component.get("v.newContact"));		
+		let action = component.get("c.addContact");
+		action.setParams({
+			"nuovo" : component.get("v.newContact")
+		});
+		action.setCallback(this, function(response) {
+			let state = response.getState();
+			if(state === "SUCCESS") {
+				console.log('Contatto creato con successo');
+				/* riempio la tabella di nuovo e
+				resetto l'attribute */
+				this.fillTableHelper(component, event);
+				component.set("v.newContact", {
+					"sObjectType": "Contact",
+					"FirstName": "",
+					"LastName": "",
+					"Email": "",
+					"Phone": ""
+				})
+			} else if(state === "ERROR") {
+				let errors = response.getError();
+				console.error(errors);
+			}
+		});
+		$A.enqueueAction(action);
+	},
+
+	deleteContactHelper : function(component, event) {
+		let action = component.get("c.deleteContactController");
+		action.setParams({
+			"contactId" : event.getSource().get("v.value")
+		});
+		action.setCallback(this, function(response) {
+			let state = response.getState();
+			if(state === "SUCCESS") {
+				this.fillTableHelper(component, event);
+			} else if(state === "ERROR") {
+				let errors = response.getError();
+				console.error(errors);
+			}
+		});
+		$A.enqueueAction(action);
 	}
 })
